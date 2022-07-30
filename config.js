d67b7ce079e978709d2dbade057a785e428d8289
rizalmhmd <92542385+rizalmhmd@users.noreply.github.com>
2022-07-30 15:57:35 +0700

    Update config.js

--- config.js
+++ config.js
@@ -132,7 +132,7 @@ global.APIs = {
 
 // key
 global.APIKeys = {
-	'https://zenzapis.xyz': 'Your Key',
+	'https://zenzapis.xyz': 'f9fccfcff1',
 }
 //menu logo maker
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
